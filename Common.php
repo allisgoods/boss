ad56d603434d1819e096f66fe15fc99456afa3b7
zhanglu <1251988523@qq.com>
2020-04-22 11:28:42 +0800

    1初始化

---
+++ Common.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\admin\Controller;
+
+use think\Controller;
+use think\Request;
+
+class Common extends Controller
+{
+    //判断用户是否登录的基类
+    public function __construct(){
+        parent::__construct();
+        $admin = session("user");
+        if($admin){
+            
+        }else{
+            //获取用户相对于公众号的唯一ID
+            $this->redirect("Login/login");
+        }
+    }
+    //多文件上传
+    public function uploads($parname){
+        $common = model("common");
+        $upre = $common->upload($parname);
+        return $upre;
+    }
+}
